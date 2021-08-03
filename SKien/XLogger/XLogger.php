0a6d017968a620203435de3b70b7979cbc2e9696
Stefan Kientzler <s.kientzler@online.de>
2021-08-03 16:05:07 +0200

    scrutinizer test...

--- SKien/XLogger/XLogger.php
+++ SKien/XLogger/XLogger.php
@@ -122,8 +122,8 @@ abstract class XLogger extends AbstractLogger
         if (strlen($strFullpath) > 0) {
             $strFullpath = $this->replacePathPlaceholder($strFullpath);
             // scrutinizer didn't realize, that pathinfo returns allways string, if $options set!
-            $this->strPath = /** @scrutinizer ignore-type */ pathinfo($strFullpath, PATHINFO_DIRNAME);
-            $this->strFilename = /** @scrutinizer ignore-type */ pathinfo($strFullpath, PATHINFO_BASENAME);
+            $this->strPath = pathinfo($strFullpath, PATHINFO_DIRNAME);
+            $this->strFilename = pathinfo($strFullpath, PATHINFO_BASENAME);
         }
     }
 
